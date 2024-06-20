0f101a41808542e2912a40d7169dad20d778e202
cute-kkiri <pbscjhj@naver.com>
2024-06-20 20:22:41 +0900

    [20240620] 강의 자료

--- bitcamp-study/myapp/app/src/main/java/bitcamp/myapp/command/BoardCommand.java
+++ bitcamp-study/myapp/app/src/main/java/bitcamp/myapp/command/BoardCommand.java
@@ -6,9 +6,6 @@ import java.util.Date;
 
 public class BoardCommand {
 
-  private static final int MAX_SIZE = 100;
-  private static Board[] boards = new Board[MAX_SIZE];
-  private static int boardLength = 0;
 
   public static void executeBoardCommand(String command) {
     System.out.printf("[%s]\n", command);
@@ -36,25 +33,26 @@ public class BoardCommand {
     board.setTitle(Prompt.input("제목?"));
     board.setContent(Prompt.input("내용?"));
     board.setCreatedDate(new Date());
-    boards[boardLength++] = board;
+    board.setNo(Board.getNextSeqNo());
+    BoardList.add(board);
   }
 
   private static void listBoard() {
     System.out.println("번호 제목 작성일 조회수");
-    for (int i = 0; i < boardLength; i++) {
-      Board board = boards[i];
+    for (Board board : BoardList.toArray()) {
       System.out.printf("%d %s %tY-%3$tm-%3$td %d\n",
-          (i + 1), board.getTitle(), board.getCreatedDate(), board.getViewCount());
+          board.getNo(), board.getTitle(), board.getCreatedDate(), board.getViewCount());
     }
   }
 
   private static void viewBoard() {
     int boardNo = Prompt.inputInt("게시글 번호?");
-    if (boardNo < 1 || boardNo > boardLength) {
+    Board board = BoardList.findByNo(boardNo);
+    if (board == null) {
       System.out.println("없는 게시글입니다.");
       return;
     }
-    Board board = boards[boardNo - 1];
+
     board.setViewCount(board.getViewCount() + 1);
     System.out.printf("제목: %s\n", board.getTitle());
     System.out.printf("내용: %s\n", board.getContent());
@@ -64,11 +62,12 @@ public class BoardCommand {
 
   private static void updateBoard() {
     int boardNo = Prompt.inputInt("게시글 번호?");
-    if (boardNo < 1 || boardNo > boardLength) {
+    Board board = BoardList.findByNo(boardNo);
+    if (board == null) {
       System.out.println("없는 게시글입니다.");
       return;
     }
-    Board board = boards[boardNo - 1];
+
     board.setViewCount(board.getViewCount() + 1);
     board.setTitle(Prompt.input("제목(%s)?", board.getTitle()));
     board.setContent(Prompt.input("내용(%s)?", board.getContent()));
@@ -77,15 +76,12 @@ public class BoardCommand {
 
   private static void deleteBoard() {
     int boardNo = Prompt.inputInt("게시글 번호?");
-    if (boardNo < 1 || boardNo > boardLength) {
+    Board deletedBoard = BoardList.delete(boardNo);
+    if (deletedBoard != null) {
+      System.out.printf("%d번 게시글을 삭제 했습니다.\n", deletedBoard.getNo());
+    } else {
       System.out.println("없는 게시글입니다.");
-      return;
     }
-    for (int i = boardNo; i < boardLength; i++) {
-      boards[i - 1] = boards[i];
-    }
-    boards[--boardLength] = null;
-    System.out.println("삭제 했습니다.");
   }
 
 }
--- bitcamp-study/myapp/app/src/main/java/bitcamp/myapp/command/ProjectCommand.java
+++ bitcamp-study/myapp/app/src/main/java/bitcamp/myapp/command/ProjectCommand.java
@@ -6,10 +6,6 @@ import bitcamp.myapp.vo.User;
 
 public class ProjectCommand {
 
-  private static final int MAX_SIZE = 100;
-  private static Project[] projects = new Project[MAX_SIZE];
-  private static int projectLength = 0;
-
   public static void executeProjectCommand(String command) {
     System.out.printf("[%s]\n", command);
     switch (command) {
@@ -41,26 +37,29 @@ public class ProjectCommand {
     System.out.println("팀원:");
     addMembers(project);
 
-    projects[projectLength++] = project;
+    project.setNo(Project.getNextSeqNo());
+
+    ProjectList.add(project);
+
     System.out.println("등록했습니다.");
   }
 
   private static void listProject() {
     System.out.println("번호 프로젝트 기간");
-    for (int i = 0; i < projectLength; i++) {
-      Project project = projects[i];
+    for (Project project : ProjectList.toArray()) {
       System.out.printf("%d %s %s ~ %s\n",
-          (i + 1), project.getTitle(), project.getStartDate(), project.getEndDate());
+          project.getNo(), project.getTitle(), project.getStartDate(), project.getEndDate());
     }
   }
 
   private static void viewProject() {
     int projectNo = Prompt.inputInt("프로젝트 번호?");
-    if (projectNo < 1 || projectNo > projectLength) {
+    Project project = ProjectList.findByNo(projectNo);
+    if (project == null) {
       System.out.println("없는 프로젝트입니다.");
       return;
     }
-    Project project = projects[projectNo - 1];
+
     System.out.printf("프로젝트명: %s\n", project.getTitle());
     System.out.printf("설명: %s\n", project.getDescription());
     System.out.printf("기간: %s ~ %s\n", project.getStartDate(), project.getEndDate());
@@ -73,11 +72,12 @@ public class ProjectCommand {
 
   private static void updateProject() {
     int projectNo = Prompt.inputInt("프로젝트 번호?");
-    if (projectNo < 1 || projectNo > projectLength) {
+    Project project = ProjectList.findByNo(projectNo);
+    if (project == null) {
       System.out.println("없는 프로젝트입니다.");
       return;
     }
-    Project project = projects[projectNo - 1];
+
     project.setTitle(Prompt.input("프로젝트명(%s)?", project.getTitle()));
     project.setDescription(Prompt.input("설명(%s)?", project.getDescription()));
     project.setStartDate(Prompt.input("시작일(%s)?", project.getStartDate()));
@@ -92,15 +92,12 @@ public class ProjectCommand {
 
   private static void deleteProject() {
     int projectNo = Prompt.inputInt("프로젝트 번호?");
-    if (projectNo < 1 || projectNo > projectLength) {
+    Project deletedProject = ProjectList.delete(projectNo);
+    if (deletedProject != null) {
+      System.out.printf("%d번 프로젝트를 삭제 했습니다.\n", deletedProject.getNo());
+    } else {
       System.out.println("없는 프로젝트입니다.");
-      return;
     }
-    for (int i = projectNo; i < projectLength; i++) {
-      projects[i - 1] = projects[i];
-    }
-    projects[--projectLength] = null;
-    System.out.println("삭제 했습니다.");
   }
 
   private static void addMembers(Project project) {
@@ -110,7 +107,7 @@ public class ProjectCommand {
         break;
       }
 
-      User user = UserCommand.findByNo(userNo);
+      User user = UserList.findByNo(userNo);
       if (user == null) {
         System.out.println("없는 팀원입니다.");
         continue;
